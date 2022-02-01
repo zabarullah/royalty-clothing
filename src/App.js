c32d663c3e28727ccca06582b2d6aa013ec525c6
zabarullah <60830173+zabarullah@users.noreply.github.com>
2022-02-01 17:10:14 +0000

    updated menu-items to navigate withRouter High Order Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+
+const Hatspage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
   <div>
-    <HomePage />  
+    <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route path='/hats' component={Hatspage} />  
+    </Switch>
   </div>
   );
 }
