06abe9e686a69d9acb6ba2230c283f49104cd067
zabarullah <60830173+zabarullah@users.noreply.github.com>
2022-02-17 19:39:52 +0000

    Converted App component to class based to handle state for the currentUser, styled the Sign in with google button & added signin/signin out functionality as a link to the header

--- src/App.js
+++ src/App.js
@@ -6,18 +6,43 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shoppage/shoppage.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
-  <div>
-    <Header />
-    <Switch>
-      <Route exact path='/' component={HomePage} />
-      <Route path='/shop' component={ShopPage} />  
-      <Route path='/signin' component={SignInAndSignUpPage} />  
-    </Switch>
-  </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+      console.log(user);
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth(); /* This will simply close the auto subscription when component unmounts */
+  }
+
+  render() {
+    return (
+    <div>
+      <Header currentUser={this.state.currentUser} />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />  
+        <Route path='/signin' component={SignInAndSignUpPage} />  
+      </Switch>
+    </div>
+    );
+
+  }
 }
 
 export default App;
--- src/components/custom-button/custom-button.component.js
+++ src/components/custom-button/custom-button.component.js
@@ -2,8 +2,8 @@ import React from "react";
 
 import './custom-button.styles.scss'
 
-const CustomButton = ({ children, ...otherProps }) => (
-    <button className="custom-button" {...otherProps}>
+const CustomButton = ({ children, isGoogleSignIn,...otherProps }) => (
+    <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}> {/* if isGoogleSignIn is true then use Google-sign-in class otherwise empty string, and in all cases the custom-botton class will be used*/}
         {children}
     </button>
 );
